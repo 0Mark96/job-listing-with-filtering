faa6efd4f898e60c959dad7715ae7d22c5362031
0mark96 <verardimarco3@gmail.com>
2022-12-16 13:52:25 +0100

    refactoring style

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,10 +1,16 @@
 import style from './Header.module.scss'
-
+import bgMobile from '../../images/bg-header-mobile.svg'
+import bgDesktop from '../../images/bg-header-desktop.svg'
 
 const Header = () => {
-    const {header} = style
+  const {header,bg_img} = style
+
   return (
     <header className={header}>
+      <picture>
+        <source media="(min-width: 376px)" srcSet={bgDesktop} />
+        <img src={bgMobile} alt="background header" className={bg_img}/>
+      </picture>
     </header>
   )
 }
